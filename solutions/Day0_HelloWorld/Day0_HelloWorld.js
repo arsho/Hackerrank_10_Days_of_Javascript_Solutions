bc1fce283c3ca78d697e4345d821df8a01e75162
arsho <shovon.sylhet@gmail.com>
2019-04-04 12:51:08 +0600

    added solution for Day 0: Hello, World

--- solutions/Day0_HelloWorld/Day0_HelloWorld.js
+++ solutions/Day0_HelloWorld/Day0_HelloWorld.js
@@ -4,3 +4,17 @@
   Author    : Ahmedur Rahman Shovon
   Created   : 04 April 2019
 */
+/**
+*   A line of code that prints "Hello, World!" on a new line is provided in the editor.
+*   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
+*
+*	Parameter:
+*   parameterVariable - A string of text.
+**/
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+    console.log(parameterVariable);
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+
+}
