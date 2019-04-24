7f9f3d2b19e44ab9915e998eb84eb5742f2d6225
arsho <shovon.sylhet@gmail.com>
2019-04-24 16:26:20 +0600

    added day3 try catch

--- solutions/Day3_TryCatchandFinally/Day3_TryCatchandFinally.js
+++ solutions/Day3_TryCatchandFinally/Day3_TryCatchandFinally.js
@@ -4,3 +4,18 @@
   Author    : Ahmedur Rahman Shovon
   Created   : 04 April 2019
 */
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ */
+function reverseString(s) {
+    try {
+        s = s.split("").reverse().join("");
+    }
+    catch (ex) {
+        console.log(ex.message)
+    }
+    finally {
+        console.log(s);
+    }    
+}
